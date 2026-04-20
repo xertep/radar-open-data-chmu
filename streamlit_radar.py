8d72f4f1dd4bdae09937d4ab03f504c62fa2d15b
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-04-20 16:50:04 +0200

    Add files via upload

--- streamlit_radar.py
+++ streamlit_radar.py
@@ -26,7 +26,7 @@ MARKERS = [
     (16.5642, 49.2147)    # second place
 ]
 
-@st.cache_data(ttl=60, show_spinner=True)
+@st.cache_data(ttl=60, show_spinner=False)
 def get_latest_radar_files(n=20):
     try:
         response = requests.get(BASE_URL, timeout=10)
@@ -52,7 +52,7 @@ def get_latest_radar_files(n=20):
         return []
 
 
-@st.cache_data(ttl=60, show_spinner=True)
+@st.cache_data(ttl=60, show_spinner="Načítám data...")
 def load_radar_images(file_urls):
     images = []
     session = requests.Session()
